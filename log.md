2a398601149a4ab3a25c629ec697ed7c7f8b222b
Paiiimannnoitla <chih870109@gmail.com>
2023-09-04 13:34:53 +0800

    Add Download function
    Add downlad function

--- log.md
+++ log.md
@@ -4,6 +4,14 @@
 	2. Add youtube downloader function
 		-- next next branch
 
+
+
+---
+	## 2023.09.04		version 0.0.1 -- Download-Function
+	-- Function implementation
+	# Added
+	1. Add file youtube download function
+		-- finished
 ---
 	## 2023.09.04		version 0.0.0 -- main
 	-- Infrastructure building
--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const {ipcMain,dialog} = require('electron')
-
+const spawn = require('child_process').spawn
+const fs = require('fs')
 ipcMain.handle('dl-main',async(event,url)=>{
 	const output = new Promise((resolve)=>{
-		console.log(url)
+		const path = `C:\\Users\\329058.D007\\Desktop\\others\\Youtube-Downloader-Portable\\yt-dlp`
+		const url = ` https://www.youtube.com/watch?v=9E6b3swbnWg`
+		const cmd = path + url
+		const powershell = spawn('powershell.exe',[cmd])
+		powershell.stdout.on('data',(i)=>{
+			console.log('success:' + i)
+		})
+		powershell.stderr.on('data',(i)=>{
+			console.log('fail:' + i)
+		})
 		resolve(url)
 	})
 	return output
